5625ddbe19b51acfb754601c5648dde2911dd36b
Tanay <tanayverma98@gmail.com>
2022-08-20 22:12:03 +0530

    Basic layout setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-import './App.css';
+import './App.css'
 
-function App() {
-  return (
-    <h1>Sanity Check</h1>
+const App = ()=>{
+  return(
+      <div>
+          <h1 className='title'>Speed Typing Game</h1>
+          <textarea 
+          name="text"
+          value=""/>
+          <h4>Time remaining </h4>
+          <button>Start Game</button>
+          <h1 className='word-count'>Word Count</h1>
+      </div>
   )
 }
 
-export default App;
+export default App
